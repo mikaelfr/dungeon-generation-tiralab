0c85d546c7712c0b1b187320a0c571a7a95ee740
Mikael <miksu.fredriksson@hotmail.com>
2020-09-05 00:58:33 +0300

    Create basic cmake project

---
+++ src/dungeon-generation.cpp
@@ -0,0 +1,12 @@
+﻿// dungeon-generation.cpp : Defines the entry point for the application.
+//
+
+#include "dungeon-generation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
