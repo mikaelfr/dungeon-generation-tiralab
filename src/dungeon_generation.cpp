bb5ce2f3908d67ecbb4168d72cbf85ccd5f9cc7f
mikaelfr <miksu.fredriksson@gmail.com>
2020-09-09 00:25:49 +0300

    Restructure code. Add testing.

--- src/dungeon_generation.cpp
+++ src/dungeon_generation.cpp
@@ -1,12 +1,15 @@
 ﻿// dungeon-generation.cpp : Defines the entry point for the application.
 //
 
-#include "dungeon-generation.h"
+#include "dungeon_generation.h"
+#include "dungeon_generation/util/Math.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+	int a;
+	cin >> a;
 	return 0;
 }
