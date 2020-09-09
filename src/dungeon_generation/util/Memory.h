70bf871ca1c454dbc0a48bbef8a212c5da2d8192
mikaelfr <miksu.fredriksson@gmail.com>
2020-09-09 15:55:37 +0300

    Fix typo

--- src/dungeon_generation/util/Memory.h
+++ src/dungeon_generation/util/Memory.h
@@ -6,11 +6,11 @@ class Memory
 {
 public:
 
-	static int MemCpy(void* const destination, rsize_t const destinationSize, void const* const source, rsize_t const sourceSize)
+	static int MemCpy(void* const destination, unsigned long const destinationSize, void const* const source, unsigned long const sourceSize)
 	{
 // GCC doesn't have memcpy_s defined, doing some hacky code gymnastics to still be able to use _s version when building testing locally
 // but using normal memcpy version when building on linux or in travis ci
-#ifdef __GNUGC__
+#ifdef __GNUC__
 		return memcpy(destination, source, sourceSize) != NULL;
 #else
 		return memcpy_s(destination, destinationSize, source, sourceSize);
