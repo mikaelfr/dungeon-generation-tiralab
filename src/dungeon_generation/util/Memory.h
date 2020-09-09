1dbf39aa0a77685c265fe5f55589cbdc19c30eb1
mikaelfr <miksu.fredriksson@gmail.com>
2020-09-09 15:49:47 +0300

    Update travis config. Use base type instead of errno_t

--- src/dungeon_generation/util/Memory.h
+++ src/dungeon_generation/util/Memory.h
@@ -6,7 +6,7 @@ class Memory
 {
 public:
 
-	static errno_t MemCpy(void* const destination, rsize_t const destinationSize, void const* const source, rsize_t const sourceSize)
+	static int MemCpy(void* const destination, rsize_t const destinationSize, void const* const source, rsize_t const sourceSize)
 	{
 // GCC doesn't have memcpy_s defined, doing some hacky code gymnastics to still be able to use _s version when building testing locally
 // but using normal memcpy version when building on linux or in travis ci
