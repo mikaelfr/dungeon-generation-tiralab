848777654a95dcf98281690a5d1aef0fac3f5e1c
mikaelfr <miksu.fredriksson@gmail.com>
2020-09-23 23:54:16 +0300

    Add tests for HasEdge

--- tests/data/TriangleTest.cpp
+++ tests/data/TriangleTest.cpp
@@ -67,6 +67,37 @@ TEST_SUITE("Test Triangle")
         CHECK(t2.HasVertFromSuperTriangle());
     }
 
+    TEST_CASE("Test HasEdge")
+    {
+        std::shared_ptr<Room> a1 = std::make_shared<Room>(1, 1, -3, 3);
+        std::shared_ptr<Room> b1 = std::make_shared<Room>(1, 1, -4, -1);
+        std::shared_ptr<Room> c1 = std::make_shared<Room>(1, 1, -2, -3);
+
+        Triangle::Edge e11 = Triangle::Edge(a1, b1);
+        Triangle::Edge e12 = Triangle::Edge(b1, c1);
+        Triangle::Edge e13 = Triangle::Edge(c1, a1);
+
+        Triangle t1(a1, b1, c1);
+
+        std::shared_ptr<Room> a2 = std::make_shared<Room>(1, -1, -2, 3);
+        std::shared_ptr<Room> b2 = std::make_shared<Room>(1, 1, 0, 1);
+        std::shared_ptr<Room> c2 = std::make_shared<Room>(-1, -1, 0, -2);
+
+        Triangle::Edge e21 = Triangle::Edge(a2, b2);
+        Triangle::Edge e22 = Triangle::Edge(b2, c2);
+        Triangle::Edge e23 = Triangle::Edge(c2, a2);
+
+        Triangle t2(a2, b2, c2);
+
+        CHECK(t1.HasEdge(e11));
+        CHECK(t1.HasEdge(e12));
+        CHECK(t1.HasEdge(e13));
+
+        CHECK(t2.HasEdge(e21));
+        CHECK(t2.HasEdge(e22));
+        CHECK(t2.HasEdge(e23));
+    }
+
     TEST_CASE("Test GetVerts")
     {
         std::shared_ptr<Room> a1 = std::make_shared<Room>(1, 1, -3, 3);
